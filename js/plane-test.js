e54776bd657e066cc2cbee648601561296c73de5
Andrew Reid <atypically.me@gmail.com>
2021-11-02 00:04:03 +0000

    Reorganized 3d vis

--- js/plane-test.js
+++ js/plane-test.js
@@ -6,6 +6,7 @@ let camera, scene, renderer, view_dist;
 let group, controls;
 let vertices, centroid, betas, resid_segments, resid_geom;
 let model_plane, model_plane_geom, model_plane_mesh, model_plane_border;
+let mean_squared_error = 0, r_squared = 0;
 
 // Load data from CSV
     Papa.parse("resources/mlr_3vars.csv", {
@@ -16,9 +17,7 @@ let model_plane, model_plane_geom, model_plane_mesh, model_plane_border;
     
         vertices = [];
         centroid = new THREE.Vector3(0,0,0);
-        
-        //console.log(results.data);
-    
+
         for (let i = 0; i < results.data.length; i++){
 		    var item = results.data[i];
 		    vertices.push(item['X1'], item['X2'], item['Y']);
@@ -31,14 +30,10 @@ let model_plane, model_plane_geom, model_plane_mesh, model_plane_border;
 
         init();
         requestAnimationFrame(animate);
-        
-//         animate();
-        
+
      } } );
 
 function init() {
-
-    //window.addEventListener( 'resize', onWindowResize, false );
 
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -70,10 +65,7 @@ function init() {
 	group.add( plane_xz );
 	group.add( plane_yz );
 	
-// 	const text_canvas = document.createElement( 'canvas' );
 	const label_font = '24px arial';
-// 	console.log(THREE.FontUtils.faces);
-	
 	group.add( UTILS.create_label_billboard( 'X1-X2', [1.1,1.1,0], 
 	                                         200, 50, 0.005, label_font,
 	                                         0xa30000) );
@@ -91,10 +83,6 @@ function init() {
 
     // Model plane
     update_model_plane();
-    
-    // TODO: add clipping planes
-    
-
     scene.add( model_plane_mesh );
     scene.add( model_plane_border );
     
@@ -102,8 +90,6 @@ function init() {
     var geometry = new THREE.BufferGeometry();
     geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
     
-    //console.log(geometry);
-   
     var sprite = new THREE.TextureLoader().load( 'js/lib/three.js-master/examples/textures/sprites/circle.png' );
    
     var material = new THREE.PointsMaterial( { color: 0xff9c38, size: .1, map: sprite,
@@ -115,39 +101,34 @@ function init() {
     scene.add( points );
     
     // Residual lines
-    var lines = get_residuals( vertices );
-    //console.log(lines);
+    var resids = UTILS.get_residuals( vertices, model_plane );
+    var lines = resids[0];
+    mean_squared_error = resids[1];
+    r_squared = resids[2];
     resid_geom = new THREE.BufferGeometry().setFromPoints( lines );
-    material = new THREE.LineBasicMaterial( { color: 0x000000, side: THREE.DoubleSide } );
-                                                 
+    material = new THREE.LineBasicMaterial( { color: 0x000000, side: THREE.DoubleSide } );                                             
     resid_segments = new THREE.LineSegments( resid_geom, material );
-
     scene.add( resid_segments );            
     
+    // Set up renderer
     var parent = $("#renderer");
-    width = parent.innerWidth();
-    
+    width = parent.innerWidth();   
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
-// 	renderer.setSize(width, 0.7*width);
-	
 	parent.append(renderer.domElement);
-	
-// 	document.body.appendChild( renderer.domElement );
-	
 	renderer.render( scene, camera );
 	
+	// Set up controls
 	createControls( camera );
-	
 	setupKeyLogger();
 	
+	// Set up info bar
 	update_info_bar();
 	
-
 }
 
 function update_model_plane( update_mesh ) {
 
-    model_plane = get_model_plane ();
+    model_plane = UTILS.get_model_plane ( betas );
     
     if (typeof model_plane_geom !== 'undefined'){
         model_plane_geom.dispose();
@@ -159,7 +140,10 @@ function update_model_plane( update_mesh ) {
         model_plane_mesh.geometry = model_plane_geom;
         model_plane_border.geometry = new THREE.EdgesGeometry( model_plane_geom );
         
-        var lines = get_residuals( vertices );
+        var resids = UTILS.get_residuals( vertices, model_plane );
+        var lines = resids[0];
+        mean_squared_error = resids[1];
+        r_squared = resids[2];
         resid_geom.dispose();
         resid_geom = new THREE.BufferGeometry().setFromPoints( lines );
         
@@ -175,21 +159,6 @@ function update_model_plane( update_mesh ) {
         }
 }
 
-function get_model_plane ( ) {
-
-    // Get normal + distance from betas
-    var denom = Math.sqrt(betas[1]*betas[1] + betas[2]*betas[2] + 1);
-    var nx = betas[1] / denom;
-    var ny = betas[2] / denom;
-    var nz = -1 / denom;
-    var normal = new THREE.Vector3(nx, ny, nz);
-    var p = betas[0] / denom;
-    
-    return new THREE.Plane( normal, p );
-    
-
-}
-
 function get_plane_geom ( plane, width, height ) {
 
     var plane_geom = new THREE.PlaneGeometry( width, height );
@@ -202,55 +171,6 @@ function get_plane_geom ( plane, width, height ) {
     plane_geom.translate(coplanarPoint.x, coplanarPoint.y, coplanarPoint.z);
 
     return plane_geom;
-
-}
-
-let mean_squared_error = 0;
-let r_squared = 0;
-
-function get_residuals( vertices ) {
-
-    // Project each point to the model plane alone z-axis
-    var offset = model_plane.normal.clone();
-    offset.normalize();
-    
-    var resids = [];
-    
-    var pt1 = new THREE.Vector3();
-    var pt2 = new THREE.Vector3();
-    var pv_pos = new THREE.Vector3(0, 0, 10);
-    var pv_neg = new THREE.Vector3(0, 0, -10);
-    var test = new THREE.Line3();
-    var pt_int = new THREE.Vector3();
-    mean_squared_error = 0;
-    var mean_squared_model_error = 0;
-    for ( let i = 0; i < vertices.length-3; i += 3 ) {
-        pt1 = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
-        pt2 = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
-        pt2.add(pv_pos);
-        test.set(pt1, pt2);
-        pt_int = new THREE.Vector3();
-        if (model_plane.intersectLine(test, pt_int) != null) {
-            resids.push(pt1, pt_int);
-            mean_squared_error += pt1.distanceToSquared(pt_int);
-            mean_squared_model_error += pt1.z*pt1.z;
-        }else {
-            pt2.set(vertices[i], vertices[i+1], vertices[i+2]);
-            pt2.add(pv_neg);
-            test.set(pt1, pt2);
-            if (model_plane.intersectLine(test, pt_int) != null) {
-                resids.push(pt1, pt_int);
-                mean_squared_error += pt1.distanceToSquared(pt_int);
-                mean_squared_model_error += pt1.z*pt1.z;
-                }
-            }
-        }
-        
-    mean_squared_error /= resids.length;
-    mean_squared_model_error /= resids.length;
-    r_squared = 1 - (mean_squared_error / mean_squared_model_error);
-    
-    return resids;
 
 }
 
@@ -306,25 +226,25 @@ function handle_event( event ) {
             return;
             
         case 'ArrowUp':
-            betas[1] += .1;
+            betas[1] += .05;
             update_model_plane();
             update_info_bar();
             return;
             
         case 'ArrowDown':
-            betas[1] -= .1;
+            betas[1] -= .05;
             update_model_plane();
             update_info_bar();
             return;
             
         case 'ArrowLeft':
-            betas[2] += .1;
+            betas[2] += .05;
             update_model_plane();
             update_info_bar();
             return;
             
         case 'ArrowRight':
-            betas[2] -= .1;
+            betas[2] -= .05;
             update_model_plane();
             update_info_bar();
             return;
@@ -387,9 +307,9 @@ $(function() {
 
     // Set up sliders
     $( "#slider_beta1" ).slider({
-      min: -2,
-      max: 2,
-      step: .1,
+      min: -1,
+      max: 1,
+      step: .05,
       value: 0.0,
       orientation: "horizontal",
       range: false,
@@ -403,9 +323,9 @@ $(function() {
     });
     
      $( "#slider_beta2" ).slider({
-      min: -2,
-      max: 2,
-      step: .1,
+      min: -1,
+      max: 1,
+      step: .05,
       value: 0.0,
       orientation: "horizontal",
       range: false,
@@ -446,8 +366,8 @@ function update_info_bar() {
 
     // Plane equation
     var eqn = '<span class="eqn-var">y&#770;</span> = <span class="eqn-val">' + 
-              betas[1].toFixed(1) + '</span><span class="eqn-var">x<sub>1</sub></span> + ' + 
-              '<span class="eqn-val">' + betas[2].toFixed(1) + 
+              betas[1].toFixed(2) + '</span><span class="eqn-var">x<sub>1</sub></span> + ' + 
+              '<span class="eqn-val">' + betas[2].toFixed(2) + 
               '</span><span class="eqn-var">x<sub>2</sub></span>';
 
     $('#info-equation').html(eqn);
--- js/utils-3d.js
+++ js/utils-3d.js
@@ -56,3 +56,64 @@ export function create_label_billboard( text, pos, width, height, scale, font, c
     return label;
     
 }
+
+export function get_model_plane ( betas ) {
+
+    // Get normal + distance from betas
+    var denom = Math.sqrt(betas[1]*betas[1] + betas[2]*betas[2] + 1);
+    var nx = betas[1] / denom;
+    var ny = betas[2] / denom;
+    var nz = -1 / denom;
+    var normal = new THREE.Vector3(nx, ny, nz);
+    var p = betas[0] / denom;
+    
+    return new THREE.Plane( normal, p );
+    
+
+}
+
+export function get_residuals( vertices, plane ) {
+
+    // Project each point to the model plane alone z-axis
+    var offset = plane.normal.clone();
+    offset.normalize();
+    
+    var resids = [];
+    
+    var pt1 = new THREE.Vector3();
+    var pt2 = new THREE.Vector3();
+    var pv_pos = new THREE.Vector3(0, 0, 10);
+    var pv_neg = new THREE.Vector3(0, 0, -10);
+    var test = new THREE.Line3();
+    var pt_int = new THREE.Vector3();
+    var mean_squared_error = 0;
+    var mean_squared_model_error = 0;
+    for ( let i = 0; i < vertices.length-3; i += 3 ) {
+        pt1 = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
+        pt2 = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
+        pt2.add(pv_pos);
+        test.set(pt1, pt2);
+        pt_int = new THREE.Vector3();
+        if (plane.intersectLine(test, pt_int) != null) {
+            resids.push(pt1, pt_int);
+            mean_squared_error += pt1.distanceToSquared(pt_int);
+            mean_squared_model_error += pt1.z*pt1.z;
+        }else {
+            pt2.set(vertices[i], vertices[i+1], vertices[i+2]);
+            pt2.add(pv_neg);
+            test.set(pt1, pt2);
+            if (plane.intersectLine(test, pt_int) != null) {
+                resids.push(pt1, pt_int);
+                mean_squared_error += pt1.distanceToSquared(pt_int);
+                mean_squared_model_error += pt1.z*pt1.z;
+                }
+            }
+        }
+        
+    mean_squared_error /= resids.length;
+    mean_squared_model_error /= resids.length;
+    var r_squared = 1 - (mean_squared_error / mean_squared_model_error);
+    
+    return [resids, mean_squared_error, r_squared];
+
+}
